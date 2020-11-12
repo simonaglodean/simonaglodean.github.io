3b1afcd493164b5a3651a780c909982181966201
Simona <simona.glodean90@gmail.com>
2020-11-12 20:53:39 +0200

    used hide instead of getElementById

--- functions.js
+++ functions.js
@@ -10,31 +10,32 @@ function hide(id) {
     }
 }
 
-function show(id){
-    console.info("show:" + id);
-    var el = document.getElementById(id);
-    console.info("show:" + id + el);
-    if (el) {
-        el.style.display = '';
-    } else {
-        console.error("elementul nu exista");
-    }
-}
+// function show(id){
+//     console.info("show:" + id);
+//     var el = document.getElementById(id);
+//     console.info("show:" + id + el);
+//     if (el) {
+//         el.style.display = '';
+//     } else {
+//         console.error("elementul nu exista");
+//     }
+// }
 
 function showHome() {
     hide("skills")
     hide("projects")
     hide("languages")
     show("home")
-    // document.getElementById('home').style.display = '';
+    document.getElementById('home').style.display = '';
 }
 
 function showSkills() {
     hide("home")
+
     hide("languages")
     hide("projects")
     show("skills")
-    // document.getElementById('skills').style.display = '';
+    document.getElementById('skills').style.display = '';
 }
 
 function showProjects() {
@@ -42,13 +43,13 @@ function showProjects() {
     hide("languages")
     hide("skills")
     show("projects")
-    // document.getElementById("projects").style.display = '';
+    document.getElementById("projects").style.display = '';
 }
 function showLanguages() {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("skills").style.display = "none";
+    hide("home")
+    hide("languages")
+    hide("skills")
     show("languages")
-    // document.getElementById("languages").style.display = '';
+    document.getElementById("languages").style.display = '';
 }
 
