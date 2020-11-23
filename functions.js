e6f8c0220071a4346ecf49c188f091b89a477b6c
Simona <simona.glodean90@gmail.com>
2020-11-23 19:53:37 +0200

    show active menu

--- functions.js
+++ functions.js
@@ -14,11 +14,15 @@ function hide(id) {
 
 function hidePreviousPage() {
     hide(activePage);
+    var link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
+    link.classList.remove("active");
 }
 
 function showPage(pageId){
     hidePreviousPage();
     document.getElementById(pageId).style.display = '';
+    var link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
+    link.classList.add("active");
     activePage = pageId;
 }
 
@@ -26,7 +30,7 @@ function initMenu(){
     document.addEventListener("click", function(e){
         var link = e.target;
         if (link.matches("#top-menu-bar a")) {
-            var id = link.innerHTML.toLowerCase();
+            var id = link.getAttribute("data-page").toLowerCase();
             showPage(id);
         } 
     })
