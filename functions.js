86fbbf47b4c200f055858e66641416ec09cb82ea
Simona <simona.glodean90@gmail.com>
2020-12-07 21:05:20 +0200

    add const and let

--- functions.js
+++ functions.js
@@ -1,8 +1,8 @@
-var activePage = "skills";
+let activePage = "skills";
 
 function hide(id) {
     console.info("hide:" + id);
-    var el = document.getElementById(id);
+    const el = document.getElementById(id);
     console.info("hide:" + id + el);
     if (el) {
         el.style.display = "none";
@@ -14,23 +14,23 @@ function hide(id) {
 
 function hidePreviousPage() {
     hide(activePage);
-    var link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
     link.classList.remove("active");
 }
 
 function showPage(pageId){
     hidePreviousPage();
     document.getElementById(pageId).style.display = '';
-    var link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
     link.classList.add("active");
     activePage = pageId;
 }
 
 function initMenu(){
     document.addEventListener("click", function(e){
-        var link = e.target;
+        const link = e.target;
         if (link.matches("#top-menu-bar a")) {
-            var id = link.getAttribute("data-page").toLowerCase();
+            const id = link.getAttribute("data-page").toLowerCase();
             showPage(id);
         } 
     })
@@ -41,16 +41,15 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    var skillsLi = skills.map(function(skill){
+    return skills.map(function(skill){
         return `<li class="${skill.endorsments > 9 ? "favorite" : ""}">
         ${skill.name}<span>&middot; ${skill.endorsments}</span>
         </li>`;
-    });
-    return skillsLi.join("");
+    }).join("");
 }
 
 function showSkills(skills){  
-    var ul = document.querySelector("#skills ul");
+    const ul = document.querySelector("#skills ul");
     ul.innerHTML = getHTMLSkills(skills);
 }
 
