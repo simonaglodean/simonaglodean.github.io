daca36415cb0de4295e43d338413e88d5e7a8648
Simona <simona.glodean90@gmail.com>
2020-12-07 21:34:03 +0200

    removed return from arrow fn

--- functions.js
+++ functions.js
@@ -41,11 +41,11 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    return skills.map(skill => {
-        return `<li class="${skill.endorsments > 9 ? "favorite" : ""}">
+    return skills.map(skill => 
+         `<li class="${skill.endorsments > 9 ? "favorite" : ""}">
         ${skill.name}<span>&middot; ${skill.endorsments}</span>
-        </li>`;
-    }).join("");
+        </li>`
+    ).join("");
 }
 
 function showSkills(skills){  
@@ -53,14 +53,12 @@ function showSkills(skills){
     ul.innerHTML = getHTMLSkills(skills);
 }
 
-fetch("data/skills.json").then(r => {
-    return r.json();
-}).then((allSkills) => {
-    allSkills.sort((s1, s2) => {
-        return s2.endorsments - s1.endorsments;
-    });
+fetch("data/skills.json")
+    .then(r => r.json())
+    .then((allSkills) => {
+        allSkills.sort((s1, s2) => s2.endorsments - s1.endorsments);
 
-    showSkills(allSkills);
+        showSkills(allSkills);
 });
 
  
